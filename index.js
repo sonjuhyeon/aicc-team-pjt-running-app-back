f787a0776e3b3426684beaf6b82efcd0be4a3d4e
sonjuhyeon <ths120521113105@daum.net>
2024-09-10 15:28:46 +0900

    edit port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); // express 모듈 불러오기
 const cors = require("cors"); // cors 모듈 불러오기
 const cookieParser = require("cookie-parser");
 
-const PORT = "8080";
+const PORT = 8080;
 
 const app = express(); // express 모듈을 사용하기 위해 app 변수에 할당
 app.use(
