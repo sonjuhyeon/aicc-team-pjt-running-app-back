e09f59276dc575052414fac3b1258f1c861f37b1
sonjuhyeon <ths120521113105@daum.net>
2024-10-01 18:27:09 +0900

    final test

--- index.js
+++ index.js
@@ -28,11 +28,21 @@ app.use(
   })
 );
 
+app.get("/", (request, response) => {
+  try {
+    response.send(
+      "Running Hi Backend Api Server. test python deploy with node"
+    );
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  }
+});
+
 app.post("/chat", (req, res) => {
   try {
     const sendQuestion = req.body.question;
     const execPython = path.join(__dirname, "bizchat.py");
-    const pythonPath = path.join(__dirname, "bin", "python3");
+    const pythonPath = path.join(__dirname, "venv", "bin", "python3");
     // console.log(sendQuestion, execPython, pythonPath);
 
     //spawn으로 파이썬 스크립트 실행
@@ -66,15 +76,48 @@ app.post("/chat", (req, res) => {
   }
 });
 
-app.get("/", (request, response) => {
-  try {
-    response.send(
-      "Running Hi Backend Api Server. test python deploy with node"
-    );
-  } catch (error) {
-    return response.status(500).json({ error: error.message });
-  }
-});
+// app.post("/chat", (req, res) => {
+//   try {
+//     const sendedQuestion = req.body.question;
+
+//     // EC2 서버에서 현재 실행 중인 Node.js 파일의 절대 경로를 기준으로 설정.
+//     const scriptPath = path.join(__dirname, "bizchat.py");
+
+//     // ec2 서버에서 실행하는 절대 경로: 개발 테스트 시 사용 불가
+//     const pythonPath = path.join(__dirname, "venv", "bin", "python3");
+
+//     // 윈도우 개발 테스트 시 사용하는 절대 경로
+//     // const pythonPath = path.join(__dirname, 'venv', 'Scripts', 'python.exe');
+
+//     // Spawn the Python process with the correct argument
+//     const result = spawn(pythonPath, [scriptPath, sendedQuestion]);
+//     let responseData = "";
+
+//     // Listen for data from the Python script
+//     result.stdout.on("data", (data) => {
+//       responseData += data.toString();
+//     });
+
+//     // Listen for errors from the Python script
+//     result.stderr.on("data", (data) => {
+//       console.error(`stderr: ${data}`);
+//       res.status(500).json({ error: data.toString() });
+//     });
+
+//     // Handle the close event of the child process
+//     result.on("close", (code) => {
+//       if (code === 0) {
+//         res.status(200).json({ answer: responseData });
+//       } else {
+//         res
+//           .status(500)
+//           .json({ error: `Child process exited with code ${code}` });
+//       }
+//     });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// });
 
 app.use(require("./routes/getRoutes"));
 app.use(require("./routes/postRoutes"));
