733710281f070500518f5808f61cff5a62eb9a93
sonjuhyeon <ths120521113105@daum.net>
2025-02-28 20:04:44 +0900

    https test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
 app.get("/", (request, response) => {
   try {
     response.send(
-      "Running Hi Backend Api Server deploy test."
+      "Running Hi Backend Api Server deploy test(https)."
     );
   } catch (error) {
     return response.status(500).json({ error: error.message });
